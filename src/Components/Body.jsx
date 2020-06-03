e7088bdd1d48c42ff6378db2fa21838d313b8256
Diégo Stéphan Jeandon Rodríguez <diego.jeandon@gmail.com>
2020-06-03 19:52:22 +0200

    :rocket: Voir Description
    - Contrôle en cas de pas avoir la géolocalisation.
    - Ajout d'un input pour effectuer des recherches ( pas encore fonctionnel ).

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import '../App.css';
 import { withCookies } from 'react-cookie';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { Row, Col } from 'react-bootstrap';
 
 class Body extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            latitude: 0,
-            longitude: 0,
+            latitude: null,
+            longitude: null,
             data: 0,
             nom: ' ',
             Main: 0,
@@ -21,24 +23,31 @@ class Body extends Component {
             ciel: 0,
             Vent: 0,
             force: 0,
-            direction: 0
+            direction: 0,
+            noGeo: false,
+            ville: ''
         };
         this.cookies = this.props.cookies;
         this.APIKEY = 'fb228059cd79a2e758028fcd08f5d067';
         this.getData = this.getData.bind(this);
         this.updateState = this.updateState.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     componentDidMount() {
-        navigator.geolocation.getCurrentPosition(async (pos) => {
+        if (!navigator.geolocation.getCurrentPosition(async (pos) => {
             await this.setState({ latitude: pos.coords.latitude });
             await this.setState({ longitude: pos.coords.longitude });
             await this.getData();
-        });
+            this.setState({ noGeo: false });
+        })) {
+            this.setState({ noGeo: true });
+        }
     }
 
     async getData() {
-        if (!this.cookies.get('MeteoWebTemporalData')) {
+        if (!this.cookies.get('MeteoWebTemporalData') || this.state.latitude === null) {
             let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.state.latitude}&lon=${this.state.longitude}&units=metric&appid=${this.APIKEY}`);
             let json = await response.json();
             await this.setState({ data: json });
@@ -62,44 +71,60 @@ class Body extends Component {
         this.setState({ force: this.state.Vent.speed });
         this.setState({ direction: (this.state.Vent.deg === undefined) ? 0 : this.state.Vent.deg });
     }
+    handleChange() {
+        this.setState({ville: document.getElementById('input').value});
+    }
+
+    handleSubmit() {
+        console.log('Ok');
+    }
 
     render() {
         return (
-            <Row>
-                <Col md={{ span: 6, offset: 3 }} xs={12}>
-                    <Row>
-                        <Col xs={12}><h1>{this.state.nom}</h1></Col>
-                    </Row>
-                    <Row>
-                        <Col xs={4}><h4>T. Max</h4></Col>
-                        <Col xs={4}><h4>Température</h4></Col>
-                        <Col xs={4}><h4>T. Min</h4></Col>
-                    </Row>
-                    <Row>
-                        <Col xs={4}><p>{this.state.temperature} ºC</p></Col>
-                        <Col xs={4}><p>{this.state.TMax} ºC</p></Col>
-                        <Col xs={4}><p>{this.state.TMin} ºC</p></Col>
-                    </Row>
-                    <Row>
-                        <Col xs={4}><h4>Humidité</h4></Col>
-                        <Col xs={4}><h4>Sensation Thermique</h4></Col>
-                        <Col xs={4}><h4>Pression</h4></Col>
-                    </Row>
-                    <Row>
-                        <Col xs={4}><p>{this.state.humidite} %</p></Col>
-                        <Col xs={4}><p>{this.state.sensation} ºC</p></Col>
-                        <Col xs={4}><p>{this.state.pression} Pa</p></Col>
-                    </Row>
-                    <Row>
-                        <Col xs={6}><h4>Force du vent</h4></Col>
-                        <Col xs={6}><p>{this.state.force} m/s</p></Col>
-                    </Row>
-                    <Row>
-                        <Col xs={6}><h4>Direction du vent</h4></Col>
-                        <Col xs={6}><p>{this.state.direction} º</p></Col>
-                    </Row>
-                </Col>
-            </Row>
+            <>
+                <Row>
+                    <Col>
+                        <input id='input' type='text' placeholder='Nom de la ville' onChange={this.handleChange} />
+                        <button id='cherche' type='submit' onClick={this.handleSubmit} ><FontAwesomeIcon icon={faSearch} /></button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={{ span: 6, offset: 3 }} xs={12}>
+                        <Row>
+                            <Col xs={12}><h1>{this.state.nom}</h1></Col>
+                        </Row>
+                        <Row>
+                            <Col sm={4} xs={3}><h4>T.Max</h4></Col>
+                            <Col sm={4} xs={6}><h4>Température</h4></Col>
+                            <Col sm={4} xs={3}><h4>T.Min</h4></Col>
+                        </Row>
+                        <Row>
+                            <Col xs={4}><p>{this.state.temperature} ºC</p></Col>
+                            <Col xs={4}><p>{this.state.TMax} ºC</p></Col>
+                            <Col xs={4}><p>{this.state.TMin} ºC</p></Col>
+                        </Row>
+                        <Row>
+                            <Col xs={4}><h4>Humidité</h4></Col>
+                            <Col xs={4}><h4>Sensation Thermique</h4></Col>
+                            <Col xs={4}><h4>Pression</h4></Col>
+                        </Row>
+                        <Row>
+                            <Col xs={4}><p>{this.state.humidite} %</p></Col>
+                            <Col xs={4}><p>{this.state.sensation} ºC</p></Col>
+                            <Col xs={4}><p>{this.state.pression} Pa</p></Col>
+                        </Row>
+                        <Row>
+                            <Col xs={6}><h4>Direction du vent</h4></Col>
+                            <Col xs={6}><h4>Force du vent</h4></Col>
+                        </Row>
+                        <Row>
+                            <Col xs={6}><p>{this.state.direction} º</p></Col>
+                            <Col xs={6}><p>{this.state.force} m/s</p></Col>
+                        </Row>
+                    </Col>
+                </Row>
+                <Row><Col>{this.state.noGeo ? <h1>La geolocation n'est pas activé</h1> : ''}</Col></Row>
+            </>
         );
     }
 } export default withCookies(Body);
