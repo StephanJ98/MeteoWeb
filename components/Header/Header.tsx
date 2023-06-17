7fd55aa6a907105ed3ad34638e5a61a4fb8ad5c2
StephanJ98 <diego.jeandon@gmail.com>
2023-06-17 13:09:21 +0200

    :zap: Search & optimization

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -8,10 +8,10 @@ type Props = {
 
 export default function Header({ darkTheme }: Props) {
     return (
-        <div className={`flex flex-wrap justify-center w-full pt-5 md:flex-row ${darkTheme ? 'text-neutral-900' : 'text-neutral-300'}`}>
+        <div className={`flex flex-wrap justify-center w-full md:w-auto md:mx-8 pt-5 md:flex-row ${darkTheme ? 'text-neutral-900' : 'text-neutral-300'} bg-neutral-100 pb-12 rounded-b-2xl`}>
             <div className={`flex flex-row justify-center text-center w-full md:w-1/2 ${!darkTheme ? 'text-neutral-900' : 'text-neutral-300'}`}>
-                <FaCloudSunRain className='w-[68px] h-[68px] min-w-[50px] min-h-[50px]' />
-                <p className='flex flex-col justify-center ml-8 font-bold text-5xl'>MeteoWeb</p>
+                <FaCloudSunRain className='w-[68px] h-[68px] min-w-[50px] min-h-[50px] text-neutral-700' />
+                <p className='flex flex-col justify-center ml-8 font-bold text-5xl text-neutral-700'>MeteoWeb</p>
             </div>
             <Search />
         </div>
